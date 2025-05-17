cd5db2a017d2d487906af62f05435d9e4ba91ae6
Livia Carvalho <livia.carvalho.lb@gmail.com>
2025-05-16 21:54:48 -0400

    feat(ui): add TaskCard and TaskCheckBox components
    This commit introduces two new UI components:
    
    - `TaskCard`: Displays task information, including title, due date, and a checkbox for completion status. It supports click actions for the card itself and the checkbox.
    - `TaskCheckBox`: A custom circular checkbox component with animated visibility for the checkmark.

---
+++ app/src/main/java/com/project/studysmart/ui/components/TaskCard.kt
@@ -0,0 +1,76 @@
+package com.project.studysmart.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.project.studysmart.domain.model.Task
+import com.project.studysmart.ui.theme.StudySmartTheme
+
+@Composable
+fun TaskCard(
+    modifier: Modifier = Modifier,
+    task: Task,
+    onClick: () -> Unit,
+    onCheckBoxClick: () -> Unit
+) {
+    ElevatedCard(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.Start),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            TaskCheckBox(task.isComplete, Color.Black) { onCheckBoxClick() }
+            Column(
+                modifier = Modifier,
+            ) {
+                Text(
+                    text = task.title,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.titleMedium,
+                    textDecoration = if (task.isComplete) {
+                        TextDecoration.LineThrough
+                    } else TextDecoration.None
+                )
+                Text(
+                    text = "${task.dueDate}",
+                    style = MaterialTheme.typography.bodySmall,
+
+                    )
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun TaskCardPrev() {
+    StudySmartTheme {
+        TaskCard(
+            task = Task("need to do", "", 4L, 3, "mat", true),
+            onClick = { },
+            onCheckBoxClick = {}
+        )
+    }
+}
