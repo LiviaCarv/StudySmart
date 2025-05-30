ab29856378b01d63e18cb7408a4aa30e249b2aaa
Livia Carvalho <livia.carvalho.lb@gmail.com>
2025-05-29 20:53:51 -0400

    feat(util): add CurrentOrFutureSelectableDates for date picker

--- app/src/main/java/com/project/studysmart/util/Common.kt
+++ app/src/main/java/com/project/studysmart/util/Common.kt
@@ -1,9 +1,13 @@
 package com.project.studysmart.util
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
 import androidx.compose.ui.graphics.Color
 import com.project.studysmart.ui.theme.Green
 import com.project.studysmart.ui.theme.Orange
 import com.project.studysmart.ui.theme.Red
+import java.time.LocalDate
+import java.time.ZoneId
 
 enum class Priority(val title: String, val color: Color, val value: Int) {
     LOW(title = "Low", color = Green, value = 0),
@@ -13,4 +17,20 @@ enum class Priority(val title: String, val color: Color, val value: Int) {
     companion object {
         fun fromInt(value: Int) = entries.firstOrNull { it.value == value } ?: MEDIUM
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+object CurrentOrFutureSelectableDates: SelectableDates {
+
+    override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+        val currentDateMillis =
+            LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
+        return utcTimeMillis >= currentDateMillis
+
+    }
+
+    override fun isSelectableYear(year: Int): Boolean {
+        return year >= LocalDate.now().year
+    }
+
 }
